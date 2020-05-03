a421795d504e0a8cb1668881f0587d71aaa500c3
Michele Marro <michele.marro@unige.ch>
2020-05-03 19:00:59 +0200

    add cumulus days to plot

--- src/readNamelist.py
+++ src/readNamelist.py
@@ -70,8 +70,16 @@ class ReadNamelist:
         lamb_new = mt.radians(float(self.lamb))
         gamma_new = mt.radians(float(self.gamma))
 
-        #day_from_1st_january
-        #...
+        #days and months
+        mon_new = [float(i) for i in self.mon_to_plot.split(' ')]
+        day_new = [float(i) for i in self.day_to_plot.split(' ')]
+        
+        #day cumulative for each mon
+        #from 0 to 334
+        cum_days = [sum(self.days_in_a_month[:i]) for i in range(len(self.days_in_a_month))]
+        
+        #sum current day_to_plot to have the shift in right days
+        cum_days_to_plot = [cum_days[j] + day_new[j] for j, i in enumerate(mon_new) if i != 0]
 
 
 if __name__ == "__main__":
